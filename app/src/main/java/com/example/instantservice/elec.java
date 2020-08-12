b998ecb1605c815606eb0df5322667cc7616c8f0
Ishfaq <180104093@aust.edu>
2020-08-12 23:23:51 +0600

    hellow 2

--- app/src/main/java/com/example/instantservice/elec.java
+++ app/src/main/java/com/example/instantservice/elec.java
@@ -2,15 +2,27 @@ package com.example.instantservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class elec extends AppCompatActivity {
 
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elec);
+        btn =(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(elec.this, notification.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/instantservice/other.java
+++ app/src/main/java/com/example/instantservice/other.java
@@ -2,13 +2,27 @@ package com.example.instantservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class other extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        btn=(Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(other.this, notification.class);
+                startActivity(intent);
+            }
+        });
     }
 }
